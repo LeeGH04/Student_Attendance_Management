3747f658268efdea89a798666dccc53c84666704
silverssol <esol04cc@gmail.com>
2024-11-11 16:39:10 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
@@ -7,9 +6,11 @@ import AttendancePage from './components/AttendancePage';
 import ProfessorMain from './components/ProfessorMain';
 import AdminMain from './components/AdminMain';
 import ParentMain from './components/ParentMain';
+//import AnnouncementPage from './components/AnnouncementsPage';  // 예시로 추가한 공지사항 페이지
+import NotificationsPage from './components/NotificationsPage';  // 알림 페이지 임포트 추가
 import PrivateRoute from './components/PrivateRoute';
-import SideBar from './components/SideBar';
 import './App.css';
+
 
 function App() {
     return (
@@ -22,9 +23,7 @@ function App() {
                     path="/student-main"
                     element={
                         <PrivateRoute>
-
-                                <StudentMain />
-
+                            <StudentMain />
                         </PrivateRoute>
                     }
                 />
@@ -38,6 +37,7 @@ function App() {
                     }
                 />
 
+                {/* 교수 전용 메인 페이지 */}
                 <Route
                     path="/professor-main"
                     element={
@@ -47,6 +47,7 @@ function App() {
                     }
                 />
 
+                {/* 학부모 전용 메인 페이지 */}
                 <Route
                     path="/parent-main"
                     element={
@@ -56,6 +57,7 @@ function App() {
                     }
                 />
 
+                {/* 관리자 전용 메인 페이지 */}
                 <Route
                     path="/admin-main"
                     element={
@@ -64,6 +66,19 @@ function App() {
                         </PrivateRoute>
                     }
                 />
+
+
+                {/* 알림 페이지 추가 */}
+                <Route
+                    path="/NotificationsPage"
+                    element={
+                        <PrivateRoute>
+                            <NotificationsPage />
+                        </PrivateRoute>
+                    }
+                />
+
+                
 
                 <Route path="/" element={<Navigate to="/login" />} />
             </Routes>
---
+++ src/components/NotificationsPage.js
@@ -0,0 +1,140 @@
+import React, { useEffect } from "react";
+import '../css/NotificationsPage.css';
+
+const NotificationsPage = () => {
+
+  useEffect(() => {
+    // 알림 권한 확인 후 알림 표시
+    if (Notification.permission === 'granted') {
+      new Notification('This is a notification!'); // new 연산자 추가
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          new Notification('This is a notification!'); // new 연산자 추가
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <div className="notifications-page">
+      <div className="text-wrapper">알림</div>
+
+      <div class="main-content">
+
+  <div class="right-container">
+    <div className="box"></div>
+  </div>
+</div>
+
+      {/* 리스트 테이블 */}
+      <table className="notification-table">
+        <thead className="first-line">
+          <tr>
+            <th>번호</th>
+            <th>보낸이</th>
+            <th>제목</th>
+            <th>읽음</th>
+            <th>답장</th>
+            <th>받은 날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>김민수</td>
+            <td> </td>
+            <td>읽기 전</td>
+            <td></td>
+            <td>2024-11-11</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>엄휘찬</td>
+            <td></td>
+            <td>읽기 전</td>
+            <td></td>
+            <td>2024-11-11</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>주사랑</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-11-10</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>이수정</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-11-8</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>최은솔</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-11-1</td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>이건휘</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-11-1</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>이원주</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-10-28</td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td>조규철</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-10-22</td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td>민정혜</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-10-21</td>
+          </tr>
+          <tr>
+            <td>10</td>
+            <td>문영준</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-9-17</td>
+          </tr>
+          <tr>
+            <td>11</td>
+            <td>최성수</td>
+            <td></td>
+            <td>읽음</td>
+            <td></td>
+            <td>2024-9-16</td>
+          </tr>
+
+          {/* 더 많은 데이터 추가 가능 */}
+        </tbody>
+      </table>
+    </div>
+
+  );
+};
+
+export default NotificationsPage;
